d7f404fe33c55ea229cc22c61bbf7eb3673024e4
yuedaxia76@gmail.com <lenovo@LAPTOP-3F8KC97O>
2018-09-17 21:57:48 +0800

    文件选择，过去太忙了，没有时间开发

--- src/main/java/org/ycalendar/ui/YCalendar.java
+++ src/main/java/org/ycalendar/ui/YCalendar.java
@@ -9,10 +9,21 @@ import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URI;
 import java.util.Calendar;
+import java.util.Iterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
@@ -20,6 +31,38 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.JToolBar;
+import javax.swing.filechooser.FileFilter;
+import net.fortuna.ical4j.data.CalendarBuilder;
+import net.fortuna.ical4j.data.CalendarOutputter;
+import net.fortuna.ical4j.data.ParserException;
+import net.fortuna.ical4j.model.Component;
+import net.fortuna.ical4j.model.Date;
+import net.fortuna.ical4j.model.DateTime;
+import net.fortuna.ical4j.model.Dur;
+import net.fortuna.ical4j.model.ParameterList;
+import net.fortuna.ical4j.model.Recur;
+import net.fortuna.ical4j.model.TimeZone;
+import net.fortuna.ical4j.model.TimeZoneRegistry;
+import net.fortuna.ical4j.model.TimeZoneRegistryFactory;
+import net.fortuna.ical4j.util.UidGenerator;
+import net.fortuna.ical4j.model.WeekDay;
+import net.fortuna.ical4j.model.component.CalendarComponent;
+import net.fortuna.ical4j.model.component.VAlarm;
+import net.fortuna.ical4j.model.component.VEvent;
+import net.fortuna.ical4j.model.component.VTimeZone;
+import net.fortuna.ical4j.model.parameter.Cn;
+import net.fortuna.ical4j.model.parameter.Role;
+import net.fortuna.ical4j.model.property.Action;
+import net.fortuna.ical4j.model.property.Attendee;
+import net.fortuna.ical4j.model.property.CalScale;
+import net.fortuna.ical4j.model.property.Description;
+import net.fortuna.ical4j.model.property.Location;
+import net.fortuna.ical4j.model.property.ProdId;
+import net.fortuna.ical4j.model.property.RRule;
+import net.fortuna.ical4j.model.property.Summary;
+import net.fortuna.ical4j.model.property.Uid;
+import net.fortuna.ical4j.model.property.Version;
+import net.fortuna.ical4j.util.UidGenerator;
 
 import org.ycalendar.dbp.service.Dictionary;
 import org.ycalendar.dbp.service.TaskService;
@@ -59,10 +102,12 @@ public class YCalendar {
         JMenuItem newTaskMe = new JMenuItem("新建任务");
         JMenuItem calMenu = new JMenuItem("日历");
         JMenuItem taskMenu = new JMenuItem("任务");
+        JMenuItem importMenu = new JMenuItem("导入...");
         eventAndTask.add(newEventme);
         eventAndTask.add(newTaskMe);
         eventAndTask.add(calMenu);
         eventAndTask.add(taskMenu);
+        eventAndTask.add(importMenu);
 
         // 这里是添加菜单
         f.setJMenuBar(jmb);
@@ -95,6 +140,196 @@ public class YCalendar {
                 areare.setSelectedIndex(1);
             }
         });
+        importMenu.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {// 只能检测到mousePressed事件
+                importFile();
+            }
+        });
+
+    }
+
+    class JAVAFileFilter extends FileFilter {
+
+        final String ext;
+
+        public JAVAFileFilter(String ext) {
+            this.ext = '.' + ext;
+        }
+
+        /* 在accept()方法中,当程序所抓到的是一个目录而不是文件时,我们返回true值,表示将此目录显示出来. */
+        @Override
+        public boolean accept(File file) {
+            if (file.isDirectory()) {
+                return true;
+            }
+            String fileName = file.getName().toLowerCase();
+            return fileName.endsWith(ext);
+
+        }
+
+        @Override
+        public String getDescription() {
+            return "只能选择" + ext + "文件";
+        }
+    }
+
+    private void importFile() {
+        JFileChooser jfc = new JFileChooser();
+        jfc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        jfc.addChoosableFileFilter(new JAVAFileFilter("ics"));//导入可选择的文件的后缀名类型
+        jfc.addChoosableFileFilter(new JAVAFileFilter("csv"));
+        jfc.showDialog(new JLabel(), "选择");
+        File file = jfc.getSelectedFile();
+        if (file.isFile()) {
+            try {
+                if (file.getName().toLowerCase().endsWith("ics")) {
+                    importIcs(file);
+                } else {
+                    importCsv(file);
+                }
+            } catch (IOException | ParserException e) {
+                JOptionPane.showMessageDialog(f, "错误:" + e.toString(), "错误", JOptionPane.ERROR_MESSAGE);
+            }
+
+        }
+
+    }
+
+    private void importIcs(File ics) throws IOException, ParserException {
+        FileInputStream fis = new FileInputStream(ics);
+        CalendarBuilder build = new CalendarBuilder();
+        net.fortuna.ical4j.model.Calendar calendar = build.build(fis);
+        for (Iterator<CalendarComponent> i = calendar.getComponents(Component.VEVENT).iterator(); i.hasNext();) {
+            VEvent event = (VEvent) i.next();
+            // 开始时间
+            System.out.println("开始时间：" + event.getStartDate().getValue());
+            // 结束时间
+            System.out.println("结束时间：" + event.getEndDate().getValue());
+            if (null != event.getProperty("DTSTART")) {
+                ParameterList parameters = event.getProperty("DTSTART").getParameters();
+                if (null != parameters.getParameter("VALUE")) {
+                    System.out.println(parameters.getParameter("VALUE").getValue());
+                }
+            }
+            // 主题
+            System.out.println("主题：" + event.getSummary().getValue());
+            // 地点
+            if (null != event.getLocation()) {
+                System.out.println("地点：" + event.getLocation().getValue());
+            }
+            // 描述
+            if (null != event.getDescription()) {
+                System.out.println("描述：" + event.getDescription().getValue());
+            }
+            // 创建时间
+            if (null != event.getCreated()) {
+                System.out.println("创建时间：" + event.getCreated().getValue());
+            }
+            // 最后修改时间
+            if (null != event.getLastModified()) {
+                System.out.println("最后修改时间：" + event.getLastModified().getValue());
+            }
+            // 重复规则
+            if (null != event.getProperty("RRULE")) {
+                System.out.println("RRULE:" + event.getProperty("RRULE").getValue());
+            }
+            // 提前多久提醒
+            for (Iterator alrams = event.getAlarms().iterator(); alrams.hasNext();) {
+                VAlarm alarm = (VAlarm) alrams.next();
+                Pattern p = Pattern.compile("[^0-9]");
+                String aheadTime = alarm.getTrigger().getValue();
+                Matcher m = p.matcher(aheadTime);
+                int timeTemp = Integer.valueOf(m.replaceAll("").trim());
+                if (aheadTime.endsWith("W")) {
+                    System.out.println("提前多久：" + timeTemp + "周");
+                } else if (aheadTime.endsWith("D")) {
+                    System.out.println("提前多久：" + timeTemp + "天");
+                } else if (aheadTime.endsWith("H")) {
+                    System.out.println("提前多久：" + timeTemp + "小时");
+                } else if (aheadTime.endsWith("M")) {
+                    System.out.println("提前多久：" + timeTemp + "分钟");
+                } else if (aheadTime.endsWith("S")) {
+                    System.out.println("提前多久：" + timeTemp + "秒");
+                }
+            }
+            // 邀请人
+            if (null != event.getProperty("ATTENDEE")) {
+                ParameterList parameters = event.getProperty("ATTENDEE").getParameters();
+                System.out.println(event.getProperty("ATTENDEE").getValue().split(":")[1]);
+                System.out.println(parameters.getParameter("PARTSTAT").getValue());
+            }
+            System.out.println("----------------------------");
+        }
+    }
+
+    private void importCsv(File ics) throws FileNotFoundException, IOException, ParserException {
+
+    }
+
+    public void exportFile() {
+        try {
+            // 创建一个时区（TimeZone）
+            TimeZoneRegistry registry = TimeZoneRegistryFactory.getInstance().createRegistry();
+            TimeZone timezone = registry.getTimeZone("Asia/Shanghai");
+            VTimeZone tz = timezone.getVTimeZone();
+
+            // 创建日历
+            net.fortuna.ical4j.model.Calendar calendar = new net.fortuna.ical4j.model.Calendar();
+            calendar.getProperties().add(new ProdId("-//Ben Fortuna//iCal4j 1.0//EN"));
+            calendar.getProperties().add(Version.VERSION_2_0);
+            calendar.getProperties().add(CalScale.GREGORIAN);
+
+            // 时间主题
+            String summary = "重复事件测试";
+            // 开始时间
+            DateTime start = new DateTime(1478016000000l);
+            // 开始时间转换为UTC时间（UTC ＋ 时区差 ＝ 本地时间 ）
+            start.setUtc(true);
+            // 结束时间
+            DateTime end = new DateTime(1478016000000l);
+            // 结束时间设置成UTC时间（UTC ＋ 时区差 ＝ 本地时间 ）
+            end.setUtc(true);
+            // 新建普通事件
+            // VEvent event = new VEvent(start, end, summary);
+            // 定义全天事件（注意默认是UTC时间）
+            VEvent event = new VEvent(new Date(1478016000000l), new Date(1478016000000l), summary);
+            event.getProperties().add(new Location("南京堵路"));
+            // 生成唯一标示
+            event.getProperties().add(new Uid("ss"));
+            // 添加时区信息
+            event.getProperties().add(tz.getTimeZoneId());
+            // 添加邀请者
+            Attendee dev1 = new Attendee(URI.create("mailto:dev1@mycompany.com"));
+            dev1.getParameters().add(Role.REQ_PARTICIPANT);
+            dev1.getParameters().add(new Cn("Developer 1"));
+            event.getProperties().add(dev1);
+            // 重复事件
+            Recur recur = new Recur(Recur.WEEKLY, Integer.MAX_VALUE);
+            recur.getDayList().add(WeekDay.MO);
+            recur.getDayList().add(WeekDay.TU);
+            recur.getDayList().add(WeekDay.WE);
+            recur.getDayList().add(WeekDay.TH);
+            recur.getDayList().add(WeekDay.FR);
+            RRule rule = new RRule(recur);
+            event.getProperties().add(rule);
+            // 提醒,提前10分钟
+            VAlarm valarm = new VAlarm(new Dur(0, 0, -10, 0));
+            valarm.getProperties().add(new Summary("Event Alarm"));
+            valarm.getProperties().add(Action.DISPLAY);
+            valarm.getProperties().add(new Description("Progress Meeting at 9:30am"));
+            // 将VAlarm加入VEvent
+            event.getAlarms().add(valarm);
+            // 添加事件
+            calendar.getComponents().add(event);
+            // 验证
+            calendar.validate();
+            FileOutputStream fout = new FileOutputStream("D://2.ics");
+            CalendarOutputter outputter = new CalendarOutputter();
+            outputter.output(calendar, fout);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private void newTask() {
